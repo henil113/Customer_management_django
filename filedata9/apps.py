fef6308f56a00ab202900d0a39343d7659628e44
Henil <39169439+henil113@users.noreply.github.com>
2020-08-13 21:54:12 -0400

    Initial Commit

---
+++ filedata9/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Filedata9Config(AppConfig):
+    name = 'filedata9'
---
+++ filedata9/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Orders
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Orders
+        fields = '__all__'
+        
+
